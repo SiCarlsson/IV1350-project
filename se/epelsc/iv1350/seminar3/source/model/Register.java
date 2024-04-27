3cbd7bdade87986832f68bce04062ee299ce5edb
Simon Carlsson <operator_ax.09@icloud.com>
2024-04-27 21:21:26 +0200

    created register and its tests

--- se/epelsc/iv1350/seminar3/source/model/Register.java
+++ se/epelsc/iv1350/seminar3/source/model/Register.java
@@ -1,9 +1,28 @@
 package se.epelsc.iv1350.seminar3.source.model;
 
 public class Register {
+  private double moneyInRegister;
+
   // Constructor
   public Register() {
-
+    this.moneyInRegister = 100;
   }
 
+  /*
+   * Updates the total amount of money in the register
+   * 
+   * @param netRecievedCash The amount of cash given by the customer when purchasing subtracted by the change amount
+   */
+  public void updateRegister(double netRecievedCash) {
+    this.moneyInRegister += netRecievedCash;
+  }
+
+  /*
+   * Function returns the amount money in the register
+   * 
+   * @return The amount of money in the register
+   */
+  public double getMoneyInRegister() {
+    return this.moneyInRegister;
+  }
 }
---
+++ se/epelsc/iv1350/seminar3/test/model/RegisterTest.java
@@ -0,0 +1,36 @@
+package se.epelsc.iv1350.seminar3.test.model;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import se.epelsc.iv1350.seminar3.source.model.Register;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class RegisterTest {
+  Register instanceToTest;
+
+  @BeforeEach
+  public void setUp() {
+    this.instanceToTest = new Register();
+  }
+
+  @AfterEach
+  public void tearDown() {
+
+  }
+
+  @Test
+  public void testUpdateRegisterCorrectIncrementation() {
+    double testedInput = 110;
+    double expectedOutput = this.instanceToTest.getMoneyInRegister() + testedInput;
+    
+    this.instanceToTest.updateRegister(testedInput);
+
+    double givenOutput = this.instanceToTest.getMoneyInRegister();
+
+    assertEquals(expectedOutput, givenOutput);
+  }
+
+}
